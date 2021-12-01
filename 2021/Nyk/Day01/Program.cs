4705aafa8712d97d5be8016c5807d955206eb2d2
CaptinBata <nbata1997@gmail.com>
2021-12-01 13:00:17 +0000

    Day01 P1 done

--- 2021/Nyk/Day01/Program.cs
+++ 2021/Nyk/Day01/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using Utils;
 
 namespace Day01
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fileReader = new FileReader();
+            var increases = 0;
+            var previousLine = 0;
+
+            foreach (var line in fileReader.ReadAllLines("input.txt"))
+            {
+                var lineNum = Int32.Parse(line);
+                if (previousLine != 0)
+                {
+                    if (previousLine < lineNum)
+                    {
+                        increases++;
+                    }
+                }
+                previousLine = lineNum;
+            }
+
+            Console.WriteLine(increases);
+            Console.ReadLine();
         }
     }
 }
---
+++ 2021/Nyk/Utils/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Utils
+{
+    public class FileReader
+    {
+        public IEnumerable<string> ReadAllLines(string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(path);
+            foreach (var line in lines)
+            {
+                yield return line;
+            }
+        }
+    }
+}
