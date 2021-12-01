a0902822dec53c586c2137f52de735f5491d306f
root <root@DESKTOP-FPM8IA0.localdomain>
2021-12-01 19:29:50 +0000

    Day 1 part 2 done, 4 stars for the gang leggo

--- 2021/Amrik/Day01/main.go
+++ 2021/Amrik/Day01/main.go
@@ -9,7 +9,17 @@ import (
 
 func main() {
 
-	file, _ := os.Open("input.txt")
+	input := "input.txt"
+
+	fmt.Println("part 1: ", partOne(input))
+	fmt.Println("part 2: ", partTwo(input))
+
+}
+
+func partOne(input string) (answer int) {
+	answer = 0
+
+	file, _ := os.Open(input)
 
 	defer file.Close()
 
@@ -17,15 +27,44 @@ func main() {
 
 	scanner.Scan()
 	prev, _ := strconv.Atoi(scanner.Text())
-	count := 0
 	for scanner.Scan() {
 		currNum, _ := strconv.Atoi(scanner.Text())
 		if currNum > prev {
-			count++
+			answer++
 		}
 		prev = currNum
 	}
 
-	fmt.Println(count)
+	return answer
 
+}
+
+func partTwo(input string) (answer int) {
+
+	file, _ := os.Open(input)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	var numbers []int
+
+	for scanner.Scan() {
+		currNum, _ := strconv.Atoi(scanner.Text())
+		numbers = append(numbers, currNum)
+	}
+
+	length := len(numbers)
+
+	prevSum := 0
+	for i, _ := range numbers {
+		currSum := 0
+		for j := i; j < i+3 && i+3 < length; j++ {
+			currSum += numbers[j]
+		}
+		if currSum > prevSum {
+			answer++
+		}
+		prevSum = currSum
+	}
+
+	return answer
 }
