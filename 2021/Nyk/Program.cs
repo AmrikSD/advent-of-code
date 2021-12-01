71cc5f6fc1c2216d97e928357a5cafb2d8290425
CaptinBata <nbata1997@gmail.com>
2021-12-01 12:08:46 +0000

    Setup repo for c#

---
+++ 2021/Nyk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Advent_Of_Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
