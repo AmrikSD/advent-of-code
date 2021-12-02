30f29371c95f8ac282b3c858c97b2bd673ff6644
CaptinBata <nbata1997@gmail.com>
2021-12-02 22:45:01 +0000

    Commented out testbed for now

--- 2021/Nyk/Utils/Utils.cs
+++ 2021/Nyk/Utils/Utils.cs
@@ -46,24 +46,24 @@ namespace Utils
         }
     }
 
-    public class Timer
-    {
-        private Stopwatch _stopwatch = new();
-        public (dynamic result, double min, double max, double avg) TimeFunction(Func<object[], dynamic> func, params object[] funcArgs)
-        {
-            var times = new List<double>();
-            dynamic value = null;
+    //public class Timer
+    //{
+    //    private Stopwatch _stopwatch = new();
+    //    public (dynamic result, double min, double max, double avg) TimeFunction(Func<object[], dynamic> func, params object[] funcArgs)
+    //    {
+    //        var times = new List<double>();
+    //        dynamic value = null;
 
-            for (var _index = 0; _index < 10; _index++)
-            {
-                _stopwatch.Reset();
-                _stopwatch.Start();
-                value = func(funcArgs);
-                _stopwatch.Stop();
-                times.Add(_stopwatch.Elapsed.TotalMilliseconds);
-            }
+    //        for (var _index = 0; _index < 10; _index++)
+    //        {
+    //            _stopwatch.Reset();
+    //            _stopwatch.Start();
+    //            value = func(funcArgs);
+    //            _stopwatch.Stop();
+    //            times.Add(_stopwatch.Elapsed.TotalMilliseconds);
+    //        }
 
-            return (value, times.Min(), times.Max(), times.Average());
-        }
-    }
+    //        return (value, times.Min(), times.Max(), times.Average());
+    //    }
+    //}
 }
