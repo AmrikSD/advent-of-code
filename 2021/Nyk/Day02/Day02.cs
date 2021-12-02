f9adf7c7bd002c5ddcb8397cad31eba3534b737d
CaptinBata <nbata1997@gmail.com>
2021-12-02 21:17:26 +0000

    Day02 completed. Part 1 and 2

---
+++ 2021/Nyk/Day02/Day02.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Utils;
+
+namespace Day02
+{
+    internal class Day02
+    {
+        static int Part1()
+        {
+            var fileReader = new FileReader();
+            var vector = new Vector(0, 0);
+
+            foreach (var line in fileReader.ReadByLines("input.txt"))
+            {
+                var directionAmount = line.Split(' ');
+                var amount = int.Parse(directionAmount[1]);
+
+                switch (directionAmount[0])
+                {
+                    case "forward":
+                        vector.X += amount;
+                        break;
+                    case "down":
+                        vector.Y += amount;
+                        break;
+                    case "up":
+                        vector.Y -= amount;
+                        break;
+                }
+            }
+
+            return vector.MultiplyAttributes();
+        }
+
+        static int Part2()
+        {
+            var fileReader = new FileReader();
+            var vector = new Vector(0, 0, 0);
+
+            foreach (var line in fileReader.ReadByLines("input.txt"))
+            {
+                var directionAmount = line.Split(' ');
+                var amount = int.Parse(directionAmount[1]);
+
+                switch (directionAmount[0])
+                {
+                    case "forward":
+                        vector.X += amount;
+                        vector.Y += vector.Aim * amount;
+                        break;
+                    case "down":
+                        vector.Aim += amount;
+                        break;
+                    case "up":
+                        vector.Aim -= amount;
+                        break;
+                }
+            }
+
+            return vector.MultiplyAttributes();
+        }
+
+        static void Main(string[] args)
+        {
+            var result1 = Part1();
+            var result2 = Part2();
+
+            Console.WriteLine($"Part 1: {result1}");
+            Console.WriteLine($"Part 2: {result2}");
+        }
+    }
+}
--- 2021/Nyk/Utils/Utils.cs
+++ 2021/Nyk/Utils/Utils.cs
@@ -27,6 +27,25 @@ namespace Utils
         }
     }
 
+    public class Vector
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Aim { get; set; }
+
+        public Vector(int x, int y, int aim = 0)
+        {
+            X = x;
+            Y = y;
+            Aim = aim;
+        }
+
+        public int MultiplyAttributes()
+        {
+            return X * Y;
+        }
+    }
+
     public class Timer
     {
         private Stopwatch _stopwatch = new();
