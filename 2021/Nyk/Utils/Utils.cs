9f5d5786ea0a05d4565009c119d14bb223787b02
CaptinBata <nbata1997@gmail.com>
2021-12-03 20:58:53 +0000

    Added in exclude for timer for unit test coverage

--- 2021/Nyk/Utils/Utils.cs
+++ 2021/Nyk/Utils/Utils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 
@@ -46,24 +47,25 @@ namespace Utils
         }
     }
 
-    //public class Timer
-    //{
-    //    private Stopwatch _stopwatch = new();
-    //    public (dynamic result, double min, double max, double avg) TimeFunction(Func<object[], dynamic> func, params object[] funcArgs)
-    //    {
-    //        var times = new List<double>();
-    //        dynamic value = null;
+    [ExcludeFromCodeCoverage]
+    public class Timer
+    {
+        private Stopwatch _stopwatch = new();
+        public (dynamic result, double min, double max, double avg) TimeFunction(Func<object[], dynamic> func, params object[] funcArgs)
+        {
+            var times = new List<double>();
+            dynamic value = null;
 
-    //        for (var _index = 0; _index < 10; _index++)
-    //        {
-    //            _stopwatch.Reset();
-    //            _stopwatch.Start();
-    //            value = func(funcArgs);
-    //            _stopwatch.Stop();
-    //            times.Add(_stopwatch.Elapsed.TotalMilliseconds);
-    //        }
+            for (var _index = 0; _index < 10; _index++)
+            {
+                _stopwatch.Reset();
+                _stopwatch.Start();
+                value = func(funcArgs);
+                _stopwatch.Stop();
+                times.Add(_stopwatch.Elapsed.TotalMilliseconds);
+            }
 
-    //        return (value, times.Min(), times.Max(), times.Average());
-    //    }
-    //}
+            return (value, times.Min(), times.Max(), times.Average());
+        }
+    }
 }
