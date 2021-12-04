2933232352354211652f53ba90e2db46166200fe
CaptinBata <nbata1997@gmail.com>
2021-12-04 19:09:27 +0000

    General cleanup

---
+++ 2021/Nyk/Utils/FileReader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Utils
+{
+    public class FileReader
+    {
+        public IEnumerable<string> ReadByLines(string path)
+        {
+            return File.ReadLines(ConvertPath(path));
+        }
+
+        public IEnumerable<string> ReadAllLines(string path)
+        {
+            return File.ReadAllLines(ConvertPath(path)).ToList();
+        }
+
+        string ConvertPath(string path)
+        {
+            return "../../../" + path;
+        }
+    }
+}
