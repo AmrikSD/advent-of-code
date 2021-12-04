2f84ef9eb94fd8e5cc7a0abb2fe723fbc8919610
root <root@DESKTOP-FPM8IA0.localdomain>
2021-12-04 06:24:16 +0000

    Day 3 part 2 done

--- 2021/Amrik/Day03/main.go
+++ 2021/Amrik/Day03/main.go
@@ -11,6 +11,7 @@ func main() {
 
 	input := "input.txt"
 	fmt.Println("part 1: ", partOne(input))
+	fmt.Println("part 2: ", partTwo(input))
 
 }
 
@@ -56,12 +57,106 @@ func partTwo(input string) (answer int) {
 	file, _ := os.Open(input)
 	scanner := bufio.NewScanner(file)
 
-	for scanner.Scan() {
+	allInput := []string{}
 
+	for scanner.Scan() {
 		in := scanner.Text()
-		fmt.Println(in)
+		allInput = append(allInput, in)
 	}
 
-	return 0
+	// Find Oxygen
+	oxygen, _ := strconv.ParseInt(findOxygen(allInput), 2, 64)
 
+	// Find Co2
+	CO2, _ := strconv.ParseInt(findCO2(allInput), 2, 64)
+	return int(oxygen) * int(CO2)
+
+}
+
+func findOxygen(input []string) string {
+
+	// Consider just the first bit.
+
+	// Keep only nunmbers selected by the bit criteria
+	// Discard numbers which aren't selected by the criteria.
+
+	// if there is only one number left, return this number.
+	// Otherwise, repeat this process, considering the next bit to the right.
+
+	i := 0
+	for len(input) > 1 {
+		input = filterByOxygenCriteria(input, i)
+		i++
+	}
+	return input[0]
+}
+
+func filterByOxygenCriteria(input []string, pos int) (out []string) {
+	// To find oxygen generator rating, determine the most common value (0 or 1) in the current bit position,
+
+	sum := 0
+	for _, v := range input {
+		if v[pos]-'0' == 1 {
+			sum++
+		}
+	}
+	bit := 0
+	if sum*2 >= len(input) {
+		bit = 1
+	}
+
+	// and keep only numbers with that bit in that position.
+	// If 0 and 1 are equally common, keep values with a 1 in the position being considered.
+
+	for _, v := range input {
+		if int(v[pos]-'0') == bit {
+			out = append(out, v)
+		}
+	}
+	return out
+}
+
+func findCO2(input []string) string {
+
+	// Consider just the first bit.
+
+	// Keep only nunmbers selected by the bit criteria
+	// Discard numbers which aren't selected by the criteria.
+
+	// if there is only one number left, return this number.
+	// Otherwise, repeat this process, considering the next bit to the right.
+
+	i := 0
+	for len(input) > 1 {
+		input = filterByCO2Criteria(input, i)
+		i++
+	}
+	return input[0]
+}
+
+func filterByCO2Criteria(input []string, pos int) (out []string) {
+	// To find oxygen generator rating, determine the most common value (0 or 1) in the current bit position,
+
+	sum := 0
+	for _, v := range input {
+		if v[pos]-'0' == 1 {
+			sum++
+		}
+	}
+	bit := 0
+	if sum*2 >= len(input) {
+		bit = 1
+	}
+	//conver to unpoopular bit
+	bit = (bit - 1) * -1
+
+	// and keep only numbers with that bit in that position.
+	// If 0 and 1 are equally common, keep values with a 1 in the position being considered.
+
+	for _, v := range input {
+		if int(v[pos]-'0') == bit {
+			out = append(out, v)
+		}
+	}
+	return out
 }
