2e9dad11c89d7e04fcbd164e5684d91d491d1522
CaptinBata <nbata1997@gmail.com>
2021-12-10 19:01:19 +0000

    Part 2 done. Thanks Amrik <3

--- 2021/Nyk/AdventOfTest/Day09/Day09Tests.cs
+++ 2021/Nyk/AdventOfTest/Day09/Day09Tests.cs
@@ -17,11 +17,11 @@ namespace AdventOfTest.Day09Tests
             Assert.AreEqual(15, result);
         }
 
-        //[Test]
-        //public void TestPart2()
-        //{
-        //    var result = Day09.Day09.Part2("Day09/TestInput.txt");
-        //    Assert.AreEqual(168, result);
-        //}
+        [Test]
+        public void TestPart2()
+        {
+            var result = Day09.Day09.Part2("Day09/TestInput.txt");
+            Assert.AreEqual(1134, result);
+        }
     }
 }
--- 2021/Nyk/Day09/Day09.cs
+++ 2021/Nyk/Day09/Day09.cs
@@ -13,6 +13,14 @@ namespace Day09
             return lines.Select(line => line.Select(numChar => int.Parse(numChar.ToString())));
         }
 
+        static (int Value, Vector Position) GetAdjacentWithCoords(IEnumerable<IEnumerable<int>> heightMap, int rowIndex, int colIndex)
+        {
+            if (rowIndex < 0 || rowIndex >= heightMap.Count() ||
+                colIndex < 0 || colIndex >= heightMap.ElementAt(0).Count())
+                return (-1, new Vector(rowIndex, colIndex));
+            else return (heightMap.ElementAt(rowIndex).ElementAt(colIndex), new Vector(rowIndex, colIndex));
+        }
+
         static int GetAdjacent(IEnumerable<IEnumerable<int>> heightMap, int rowIndex, int colIndex)
         {
             if (rowIndex < 0 || rowIndex >= heightMap.Count() ||
@@ -21,12 +29,9 @@ namespace Day09
             else return heightMap.ElementAt(rowIndex).ElementAt(colIndex);
         }
 
-        public static int Part1(string inputFile = "input.txt")
+        static List<(int Value, Vector Position)> CalculateLowPoints(IEnumerable<IEnumerable<int>> heightMap)
         {
-            var fileReader = new FileReader();
-            var heightMap = ParseInput(fileReader.ReadAllLines(inputFile));
-            var lowestPoints = new List<int>();
-
+            var lowPoints = new List<(int, Vector)>();
             for (var row = 0; row < heightMap.Count(); row++)
             {
                 for (var col = 0; col < heightMap.ElementAt(row).Count(); col++)
@@ -36,22 +41,90 @@ namespace Day09
                     lowestPointRow.Add(GetAdjacent(heightMap, row - 1, col)); //up
                     lowestPointRow.Add(GetAdjacent(heightMap, row + 1, col)); //down
                     lowestPointRow.Add(GetAdjacent(heightMap, row, col - 1)); //left
-                    lowestPointRow.Add(GetAdjacent(heightMap, row, col + 1)); // right
+                    lowestPointRow.Add(GetAdjacent(heightMap, row, col + 1)); //right
 
                     lowestPointRow.RemoveAll(ints => ints == -1);
                     var min = lowestPointRow.Min();
 
                     if (min > heightMap.ElementAt(row).ElementAt(col))
-                        lowestPoints.Add(heightMap.ElementAt(row).ElementAt(col) + 1);
+                        lowPoints.Add((heightMap.ElementAt(row).ElementAt(col) + 1, new Vector(row, col)));
+                }
+            }
+            return lowPoints;
+        }
+
+        public static int Part1(string inputFile = "input.txt")
+        {
+            var fileReader = new FileReader();
+            var heightMap = ParseInput(fileReader.ReadAllLines(inputFile));
+
+            var lowPoints = CalculateLowPoints(heightMap);
+
+            return lowPoints.Select(tuple => tuple.Value).Sum();
+        }
+
+        static List<Vector> DiscoverBasin(IEnumerable<IEnumerable<int>> heightMap, Vector lowPoint, List<Vector> visitedPositions = null)
+        {
+            var pointsInBasin = new List<Vector>();
+            var neighbours = new List<(int Value, Vector Position)>();
+
+            neighbours.Add(GetAdjacentWithCoords(heightMap, lowPoint.X - 1, lowPoint.Y)); //up
+            neighbours.Add(GetAdjacentWithCoords(heightMap, lowPoint.X + 1, lowPoint.Y)); //down
+            neighbours.Add(GetAdjacentWithCoords(heightMap, lowPoint.X, lowPoint.Y - 1)); //left
+            neighbours.Add(GetAdjacentWithCoords(heightMap, lowPoint.X, lowPoint.Y + 1)); //right
+
+            neighbours.RemoveAll(neighbour => neighbour.Item1 == -1);
+
+            if (visitedPositions == null)
+            {
+                visitedPositions = new List<Vector>();
+            }
+
+            pointsInBasin.Add(lowPoint);
+            visitedPositions.Add(lowPoint);
+
+            foreach (var neighbour in neighbours)
+            {
+                var visited = visitedPositions
+                    .Select(position => position == neighbour.Position)
+                    .Where(isVisited => isVisited == true).Count() > 0;
+
+                if (!visited)
+                {
+                    var neighbourPosition = neighbour.Position;
+                    if (heightMap.ElementAt(neighbourPosition.X).ElementAt(neighbourPosition.Y) != 9)
+                    {
+                        pointsInBasin.AddRange(DiscoverBasin(heightMap, neighbourPosition, visitedPositions));
+                    }
                 }
             }
 
-            return lowestPoints.Sum();
+            return pointsInBasin;
+        }
+
+        static List<List<Vector>> CalculateBasins(IEnumerable<IEnumerable<int>> heightMap, List<Vector> lowPoints)
+        {
+            var basins = new List<List<Vector>>();
+            foreach (var lowPoint in lowPoints)
+            {
+                basins.Add(DiscoverBasin(heightMap, lowPoint));
+            }
+
+            return basins;
         }
 
         public static long Part2(string inputFile = "input.txt")
         {
-            return 0;
+            var fileReader = new FileReader();
+            var heightMap = ParseInput(fileReader.ReadAllLines(inputFile));
+
+            var lowPoints = CalculateLowPoints(heightMap);
+            var lowPointPositions = lowPoints.Select(tuple => tuple.Position).ToList();
+            var basins = CalculateBasins(heightMap, lowPointPositions).OrderByDescending(basin => basin.Count());
+
+            var topThreeBasins = basins.Take(3);
+
+            return topThreeBasins.Select(basin => basin.Count).Aggregate((basin1, basin2) => basin1 * basin2);
         }
 
         [ExcludeFromCodeCoverage]
