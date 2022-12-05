64bdf0ecadcf02f0ad02f0958a9c6e4e72ed1951
AmrikSD <Amrik.Singh@live.co.uk>
2022-12-05 19:32:07 +0000

    day 3 done

--- 2022/Amrik/src/main/java/xyz/amrik/adventofcode/App.java
+++ 2022/Amrik/src/main/java/xyz/amrik/adventofcode/App.java
@@ -2,17 +2,16 @@ package xyz.amrik.adventofcode;
 
 import xyz.amrik.adventofcode.Day.*;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Day[] days = new Day[]{
-            new DayOne("./resources/day01.txt"),
-            new DayTwo("./resources/day02.txt")
+public class App {
+    public static void main(String[] args) {
+        Day[] days = new Day[] {
+                new DayOne("./resources/day01.txt"),
+                new DayTwo("./resources/day02.txt"),
+                new DayThree("./resources/day03.txt")
         };
         System.out.format("%3s%15s%15s%n", "Day", "Part 1", "Part 2");
         for (int i = 0; i < days.length; i++) {
-            System.out.format("%3s%15s%15s%n", i+1, days[i].answerOne, days[i].answerTwo);
+            System.out.format("%3s%15s%15s%n", i + 1, days[i].answerOne, days[i].answerTwo);
         }
     }
 }
