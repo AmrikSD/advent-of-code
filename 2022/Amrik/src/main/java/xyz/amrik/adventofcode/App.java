a9b58c24123ba50db558410a8036d18944a9887f
AmrikSD <Amrik.Singh@live.co.uk>
2022-12-05 20:09:35 +0000

    Day 4 done

--- 2022/Amrik/src/main/java/xyz/amrik/adventofcode/App.java
+++ 2022/Amrik/src/main/java/xyz/amrik/adventofcode/App.java
@@ -7,7 +7,8 @@ public class App {
         Day[] days = new Day[] {
                 new DayOne("./resources/day01.txt"),
                 new DayTwo("./resources/day02.txt"),
-                new DayThree("./resources/day03.txt")
+                new DayThree("./resources/day03.txt"),
+                new DayFour("./resources/day04.txt")
         };
         System.out.format("%3s%15s%15s%n", "Day", "Part 1", "Part 2");
         for (int i = 0; i < days.length; i++) {
