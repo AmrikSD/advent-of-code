56769e3eda82acf164eb8db97229151857f0ab2b
Nykolas Bata <nbata1997@gmail.com>
2024-12-01 22:32:50 +0000

    2024 Day 1 Completed

--- 2024/benchmarks_test.go
+++ 2024/benchmarks_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	amrik "2024/amrik"
+	nyk "2024/nyk/day1"
 	"testing"
 )
 
@@ -10,7 +11,7 @@ func BenchmarkAmrikDay1Part1(b *testing.B) {
 	amrik.Day01Part01("./amrik/inputs/day01/input.txt")
 }
 func BenchmarkNykDay1Part1(b *testing.B) {
-
+	nyk.Part1("./nyk/day1/input.txt")
 }
 
 // Day 1 Part 2
@@ -18,7 +19,7 @@ func BenchmarkAmrikDay1Part2(b *testing.B) {
 	amrik.Day01Part02("./amrik/inputs/day01/input.txt")
 }
 func BenchmarkNykDay1Part2(b *testing.B) {
-
+	nyk.Part2("./nyk/day1/input.txt")
 }
 
 // Day 2 Part 1
